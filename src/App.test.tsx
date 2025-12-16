6e35fc0d1688da766711618857a4364a48259b88
Andrew Guard <Andrew.Guard@red-gate.com>
2025-12-16 11:16:06 -0600

    package lock

--- src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders Flyway Dashboard', () => {
-  const { container } = render(<App />);
-  expect(container).toBeInTheDocument();
-});
